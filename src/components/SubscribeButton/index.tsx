06caa5f81b43ad6aec519adc51ac3a4b491b1a88
igor-cotrim <igorxuxicotrim@gmail.com>
2021-03-30 21:50:42 -0300

    config to pay with stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,16 +1,38 @@
-import styles from './styles.module.scss'
+import { useSession, signIn } from "next-auth/client";
 
-interface SubscribeButtonProps{
+import { api } from "../../services/api";
+import { getStipeJs } from "../../services/stripe-js";
+
+import styles from "./styles.module.scss";
+
+interface SubscribeButtonProps {
   priceId: string;
 }
 
-export function SubscribeButton({priceId}: SubscribeButtonProps) {
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("gitHub");
+      return;
+    }
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStipeJs();
+
+      await stripe.redirectToCheckout({sessionId});
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
-      Subscribe now
+      Assine Já!
     </button>
-  )
+  );
 }
